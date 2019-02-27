d68196d3849e22e168de0654c3a5aabcbaf41171
jjshawshaw <jshaw00@stuy.edu>
2019-02-27 11:06:04 -0500

    added empty class + dat files

--- ReadFile.java
+++ ReadFile.java
@@ -5,10 +5,18 @@ public class ReadFile{
   public static void main(String[]args){
     try {
       File f = new File("Maze1.txt");
+
+      Scanner infLen = new Scanner(f);
+      int rowLen = 0;
+      int colLen = 0;
+      while (infLen.hasNextLine()){
+        rowLen += 1;
+        colLen = infLen.nextLine().length();
+      }
+      char[][] grid = new char[rowLen][colLen];
       Scanner inf = new Scanner(f);
       while (inf.hasNextLine()){
-        String line = inf.nextLine();
-        System.out.println(line);
+
       }
     }
     catch (FileNotFoundException e){
